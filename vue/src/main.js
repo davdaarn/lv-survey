5cb68caa0285832ed0e6e59b23996296aa88652c
David Long <davdaarn@live.com>
2022-02-20 12:29:35 -0700

    init vue router and routes with tailwind components

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import store from './store'
+import router from './router'
 import App from './App.vue'
 
 import './index.css'
 
 createApp(App)
   .use(store)
+  .use(router)
   .mount('#app')
